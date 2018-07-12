8ca28b4be11d00227015fcced13f026963827424
GabrielPaliari <gabriel.paliari@gmail.com>
2018-07-11 21:24:57 -0300

    Biblioteca Material Kit React Integrada

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import { List, ListItem, withStyles } from "material-ui";
+
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+
+import footerStyle from "assets/jss/material-kit-react/components/footerStyle.jsx";
+
+function Footer({ ...props }) {
+  const { classes, whiteFont } = props;
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  const aClasses = classNames({
+    [classes.a]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#" className={classes.block}>
+                projeto
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+              >
+                Sobre Nós
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <div className={classes.right}>
+          &copy; {1900 + new Date().getYear()} , Feito com{" "}
+          <Favorite className={classes.icon} /> por{" "}
+          Gabriel Paliari e Victor Funabashi.
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  whiteFont: PropTypes.bool
+};
+
+export default withStyles(footerStyle)(Footer);
